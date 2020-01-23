049c385e28fb254c2bed1c6679f7fdb1401c1005
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-23 08:02:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,9 +16,10 @@ def print_first_directors_movie_titles
 nds = directors_database
 ssindex=nds.index('Stephen Speilberg') #speilberg's array index
 nsm = nds[ssindex].length #number of Speilberg movies
+puts nsm
 a=[]
 i=0
-while i<nsm do
+while i < nsm do
   a[i]=nds[ssindex][i][:title]
 i=i+1
 end
